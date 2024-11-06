ca6b5e8b0d209df211dcc73345985badd780388b
Ghassan Hallaq <gus.hallaq@gmail.com>
2024-11-06 04:33:00 -0500

    code init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/', function (request, response) {
+    const filePath = path.join(__dirname, 'print.html');
+    response.sendFile(filePath);
+});
+
+app.get('/print', function (request, response) {
+    const filePath = path.join(__dirname, 'receipt.html');
+    response.sendFile(filePath);
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
