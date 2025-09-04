5aa079a4554b7d2b6d88e3abbe3a13f72f291a15
Ghassan Hallaq <ghallaq@redventures.com>
2025-09-04 15:09:44 -0400

    printing page

--- src/app.js
+++ src/app.js
@@ -3,15 +3,20 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// app.get('/', function (request, response) {
+//     const filePath = path.join(__dirname, 'print.html');
+//     response.sendFile(filePath);
+// });
+
 app.get('/', function (request, response) {
-    const filePath = path.join(__dirname, 'print.html');
+    const filePath = path.join(__dirname, 'printing.html');
     response.sendFile(filePath);
 });
 
-app.get('/print', function (request, response) {
-    const filePath = path.join(__dirname, 'receipt.html');
-    response.sendFile(filePath);
-});
+// app.get('/print', function (request, response) {
+//     const filePath = path.join(__dirname, 'receipt.html');
+//     response.sendFile(filePath);
+// });
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
